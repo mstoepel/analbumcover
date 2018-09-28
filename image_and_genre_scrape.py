16ccedeb5532efee689aa819c9262832ec14dfa6
mstoepel <mstoepel@gmail.com>
2018-09-28 18:09:47 -0400

    new file scheme
    updating with new new file scheme

--- image_and_genre_scrape.py
+++ image_and_genre_scrape.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Nov 26 12:16:29 2016
-
 @author: michael
 """
 
 from bs4 import BeautifulSoup
-import urlparse
-from urllib import urlretrieve
-import urllib2
+import urllib
+import os
 
 #url = 'http://newalbumreleases.net/category/metal/'
 #url = 'http://newalbumreleases.net/category/metal/page/10'
 #url = 'http://newalbumreleases.net/category/rock/'
 
 url_list = ['http://newalbumreleases.net/category/metal/']
-for n in range(30):
+for n in range(4):
     url_list.append('http://newalbumreleases.net/category/metal/page/'+str(n+1))
 
-for n in range(30):
-    soup = BeautifulSoup(urllib2.urlopen(url_list[n]))
+for n in range(4):
+    soup = BeautifulSoup(urllib.request.urlopen(url_list[n]))
     img_links = soup.findAll("div", {"class":"entry"})
 
 # THIS DOWNLOADS THE IMAGES, AND NAMES THEM BASED ON THEIR URL. WANT TO NAME THEM BASED ON ALBUM_GENRE
@@ -35,9 +33,15 @@ for n in range(30):
         str2 = str1[str1.find(art)+11:str1.find(st2,str1.find(art))]
         str3 = str1[str1.find(alb)+10:str1.find(st2,str1.find(alb))]
         str4 = str1[str1.find(st)+7:str1.find(st2,str1.find(st))]
-        file_name = "album covers/" + str2.replace(" ","") + "_" + str3.replace(" ","") + "_" + str4.replace(" ","") +".jpg"
-        print file_name
-        urlretrieve(img_url,file_name)
+        # file_name = "c:/ML/analbumcover/test/" + str2.replace(" ","") + "_" + str3.replace(" ","") + "_" + str4.replace(" ","") +".jpg"
+        file_base = 'c:/ML/analbumcover/album_covers/{}'.format(str4.replace(' ',''))
+        print(file_base)
+        file_name = "c:/ML/analbumcover/album_covers/{2}/{0}_{1}.jpg".format(str2.replace(' ',''), str3.replace(' ',''), str4.replace(' ', ''))
+        if not os.path.exists(file_base):
+            print('making it')
+            os.makedirs(file_base)
+        print(file_name)
+        urllib.request.urlretrieve(img_url, file_name)
 
 
 
